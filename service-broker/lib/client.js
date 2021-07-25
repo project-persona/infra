bfc54f853074839b28f8be3c776f98228865078a
tabjy <admin@tabjy.com>
2021-07-24 17:32:55 -0700

    extract RPC timeout to config, increase value to 10s

--- service-broker/lib/client.js
+++ service-broker/lib/client.js
@@ -2,7 +2,7 @@ const { Request } = require('zeromq')
 
 const { Header } = require('./types')
 
-const { BROKER_ADDR } = require('./config')
+const { BROKER_ADDR, RECEIVE_TIMEOUT } = require('./config')
 
 module.exports = class Client {
   constructor (address = BROKER_ADDR) {
@@ -11,7 +11,7 @@ module.exports = class Client {
 
   async request (service, ...req) {
     // XXX: we rely on zmq router to distinguish different requests by the same client.
-    const socket = new Request({ receiveTimeout: 2000 }) // TODO: extract constants to config?
+    const socket = new Request({ receiveTimeout: RECEIVE_TIMEOUT })
     socket.connect(this.address)
 
     console.log(`Requesting service '${service}' with ${req.reduce((acc, cur) => acc + cur.length, 0)} bytes payload`)
--- service-broker/lib/config.js
+++ service-broker/lib/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  BROKER_ADDR: process.env.BROKER_ADDR || 'tcp://0.0.0.0:5555'
+  BROKER_ADDR: process.env.BROKER_ADDR || 'tcp://0.0.0.0:5555',
+  RECEIVE_TIMEOUT: parseInt(process.env.RECEIVE_TIMEOUT, 10) || 10000
 }
